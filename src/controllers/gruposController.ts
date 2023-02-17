784e4f77a428349127b22e11271d8538d01a7ec0
SrzzuareS <srzzuares@gmail.com>
2023-02-16 22:13:04 -0600

    gruFinish

--- src/controllers/gruposController.ts
+++ src/controllers/gruposController.ts
@@ -1,79 +1,100 @@
-import { Express } from "express";
+import { Request, Response } from "express";
 import { PrismaClient, grupos } from "@prisma/client";
 const prisma = new PrismaClient()
 
 // Grupos Crea un grupo
-const crearGrupos = async (req: Express.Request, res: Express.Response) => {
-    //const {} = null
+const crearGrupos = async (req: Request, res: Response) => {
+    const {Estatus, Grado, Grupo, IdCarrera, IdPeriodo} = req.body
     const postGru = await prisma.grupos.create({
         data: {
-            Estatus: 'activo',
-            Grado: '8',
-            Grupo: 'A',
-            IdCarrera: 1,
-            IdPeriodo: 1,
+            Estatus,
+            Grado,
+            Grupo,
+            IdCarrera,
+            IdPeriodo,
         }
     })
-    console.log('Se Creo un Grupo: ', postGru)
+    res.status(200).json({
+        menssage: " Creado _-",
+        success: true,
+        data: postGru
+    })
 }
 
 
 // Grupos Obtener todo los grupos registrados
-const obtenerGrupos = async (req: Express.Request, res: Express.Response) => {
-    //const {} = null
+const obtenerGrupos = async (req: Request, res: Response) => {
     const getGru = await prisma.grupos.findMany()
-    console.log('Se obtiene los datos de Grupo: ', getGru)
+    res.status(200).json({
+        menssage:"Resultados Encontrados ",
+        success: true,
+        data: getGru
+    })
 }
 
 
 // Grupos 
-const obtenerOneGrupos = async (req: Express.Request, res: Express.Response) => {
-    //const {} = null
+const obtenerOneGrupos = async (req: Request, res: Response) => {
+    const idGrupos = Number(req.params.idGrupos)
     const getOneGru = await prisma.grupos.findUnique({
         where: {
-            idGrupos: 1
+            idGrupos
         }
     })
-    console.log('Se Obtiene por id de un Grupo: ', getOneGru)
+    res.status(200).json({
+        menssage: "Encontrado ",
+        success: true,
+        data: getOneGru
+    })
 }
 
 
 // Grupos 
-const actualizarGrupos = async (req: Express.Request, res: Express.Response) => {
-    //const {} = null
+const actualizarGrupos = async (req: Request, res: Response) => {
+    const idGrupos = Number(req.params.idGrupos)
+    const {Estatus, Grado, Grupo, IdCarrera, IdPeriodo} = req.body
     const actGru = await prisma.grupos.update({
         where: {
-            idGrupos: 1,
+            idGrupos
         },
         data: {
-            Estatus: 'inactivo',
-            Grado: '8',
-            Grupo: 'A',
-            IdCarrera: 1,
-            IdPeriodo: 1,
+            Estatus,
+            Grado,
+            Grupo,
+            IdCarrera,
+            IdPeriodo,
         }
     })
-    console.log('Se Actualizo un dato de Grupo: ', actGru)
+    res.status(200).json({
+        menssage: "Actualizado",
+        success: true,
+        data: actGru
+    })
 }
 
 
 // Grupos 
-const eliminarGrupos = async (req: Express.Request, res: Express.Response) => {
-    //const {} = null
+const eliminarGrupos = async (req: Request, res: Response) => {
+    const idGrupos = Number(req.params.idGrupos)
     const postGru = await prisma.grupos.delete({
         where: {
-            idGrupos: 1
+            idGrupos
         }
     })
-    console.log('Se Elimino un dato de Grupo: ', postGru)
+    res.status(200).json({
+        menssage: "Se elimino un registro de grupos",
+        success: true
+    })
 }
 
 
 // Grupos 
-const eliminarTodoGrupos = async (req: Express.Request, res: Express.Response) => {
-    //const {} = null
+const eliminarTodoGrupos = async (req: Request, res: Response) => {
+    
     const postGru = await prisma.grupos.deleteMany()
-    console.log('Se Elimito todo de Grupos: ', postGru)
+    res.status(200).json({
+        menssage: "Se elimino todo registro de grupos"
+    })
 }
 
 
